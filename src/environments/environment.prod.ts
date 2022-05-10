442dced3ded5ed0c1b243d1543e27c0e8579d393
Oliver Mänder <oliver.maender@gmail.com>
2022-05-10 15:43:01 +0200

    changed some design details and added documentation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBz_H3_cuELo1d3gOCppzWL1q6mg1NMa5s",
+    authDomain: "commplat-960d7.firebaseapp.com",
+    projectId: "commplat-960d7",
+    storageBucket: "commplat-960d7.appspot.com",
+    messagingSenderId: "559144752655",
+    appId: "1:559144752655:web:7d434340aff87e4006b57a"
+  }
 };
